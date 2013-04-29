6b3ad8c641841ff274a7f6cc24daa52b844dc991
weiserj <>
2013-04-29 19:36:30 +0000

    *** empty log message ***

--- src/org/reactome/diagram/client/CanvasPopupMenu.java
+++ src/org/reactome/diagram/client/CanvasPopupMenu.java
@@ -197,7 +197,7 @@ public class CanvasPopupMenu extends PopupPanel {
     					diagramPane.getInteractorCanvas().removeProtein(pSelected);
     					pSelected.setDisplayingInteractors(false);
     				} else {	
-    					diagramPane.getController().getInteractors(pSelected);
+    					diagramPane.getController().getInteractors(pSelected, Boolean.TRUE);
     					pSelected.setDisplayingInteractors(true);
     				}
     				hide();
