d0bdccc36d237a16748b744d3ba8d13502864f1f
weiserj <>
2013-08-19 17:20:58 +0000

    Added method for removing widget borders

--- src/org/reactome/diagram/client/WidgetStyle.java
+++ src/org/reactome/diagram/client/WidgetStyle.java
@@ -4,6 +4,8 @@
  */
 package org.reactome.diagram.client;
 
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -14,6 +16,14 @@ import com.google.gwt.user.client.ui.Widget;
 public class WidgetStyle {
     
     public static void bringToFront(Widget widget) {
-    	widget.getElement().getStyle().setZIndex(2);
+    	getStyle(widget).setZIndex(2);
+    }
+    
+    public static void removeBorder(Widget widget) {
+    	getStyle(widget).setBorderWidth(0, Unit.PX);
+    }
+    
+    private static Style getStyle(Widget widget) {
+    	return widget.getElement().getStyle();
     }
 }    
