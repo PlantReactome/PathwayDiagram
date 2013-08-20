d22153ab696129d5a9fb1d89a72ce555551ea08c
weiserj <>
2013-08-20 21:09:22 +0000

    Shows info icon when hovering over nodes

--- src/org/reactome/diagram/client/PathwayCanvasHoverHandler.java
+++ src/org/reactome/diagram/client/PathwayCanvasHoverHandler.java
@@ -7,9 +7,23 @@ package org.reactome.diagram.client;
 import java.util.List;
 
 import org.reactome.diagram.model.GraphObject;
+import org.reactome.diagram.model.GraphObjectType;
+import org.reactome.diagram.model.Node;
 
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.dom.client.Style.Cursor;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.touch.client.Point;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.PopupPanel.PositionCallback;
 
 /**
  * This class is used to handle hovering over objects for PathwayDiagramPanel.
@@ -18,10 +32,12 @@ import com.google.gwt.user.client.ui.Label;
  */
 public class PathwayCanvasHoverHandler extends HoverHandler {
     private PathwayCanvas pc;
-	
+	private InfoIcon infoIcon;
+    
     public PathwayCanvasHoverHandler(PathwayDiagramPanel diagramPanel, PathwayCanvas pathwayCanvas) {
         super(diagramPanel, pathwayCanvas);
         pc = pathwayCanvas; 
+        infoIcon = new InfoIcon();
     }
 
     public GraphObject hover(Point hoverPoint) {
@@ -33,17 +49,138 @@ public class PathwayCanvasHoverHandler extends HoverHandler {
         List<GraphObject> objects = pc.getPathway().getGraphObjects();
         super.hover(objects);        
         
-        if (hoveredObject != null && (!(isOverSameObject && timeElapsed)))
-        	showTooltip();
-        
+        if (hoveredObject != null) {
+        	infoIcon.hide();
+        	showPopups();
+        } else {
+        	infoIcon.startHideTimer();
+        }
+        	
         return hoveredObject;        
     }
-        
-    protected void showTooltip() {
-    	String label = super.getLabel();
-    	 	
-    	tooltip.setWidget(new Label(label));
     
-    	super.showTooltip();
+    private void showPopups() {
+    	if (hoveredObject instanceof Node)
+    		showInfoIcon((Node) hoveredObject);
+    	showTooltip();
     }
+    
+    private void showInfoIcon(final Node entity) {
+    	infoIcon.stopHideTimer();
+    	infoIcon.setEntity(entity);
+    	infoIcon.setPopupPositionAndShow(new PositionCallback(){
+			
+    		@Override
+			public void setPosition(int offsetWidth, int offsetHeight) {
+				final Integer OFFSET = 2;
+				
+				final Integer popupLeft = pc.getAbsoluteXCoordinate(
+											entity.getBounds().getX() + entity.getBounds().getWidth() - OFFSET
+										  );
+				final Integer popupTop = pc.getAbsoluteYCoordinate(
+											entity.getBounds().getY() + OFFSET
+										 ) - offsetHeight;
+				infoIcon.setPopupPosition(popupLeft, popupTop);
+			}    		
+    	});
+    }
+    
+    protected void showTooltip() {
+    	if (hoveredObject.getType() == GraphObjectType.RenderableReaction) {
+    		String label = super.getLabel();
+    	 	
+    		tooltip.setWidget(new Label(label));
+    
+    		super.showTooltip();
+    	}
+    }
+    
+    interface Resources extends ClientBundle {
+		@Source("InfoIcon.png")
+		ImageResource infoIcon();
+	}
+	
+	protected class InfoIcon extends PopupPanel {
+		private Image infoIcon;
+		private Node entity;
+		private HideTimer hideTimer;
+		
+		public InfoIcon() {
+			infoIcon = new Image(((Resources) GWT.create(Resources.class)).infoIcon());
+			setWidget(infoIcon);
+			WidgetStyle.bringToFront(this);
+			WidgetStyle.removeBorder(this);
+			
+			addHandlers();
+			addHideTimer();
+		}
+		
+		public Image getInfoIconImage() {
+			return infoIcon;
+		}
+		
+		public void setEntity(Node entity) {
+			this.entity = entity;
+		}
+		
+		public Node getEntity() {
+			return entity;
+		}
+					
+		public void startHideTimer() {
+			hideTimer.schedule(300);
+		}
+		
+		public void stopHideTimer() {
+			hideTimer.cancel();
+		}
+				
+		private void addHandlers() {
+			addClickHandler();
+			addMouseOverHandler();
+		}
+		
+		private void addClickHandler() {
+			infoIcon.addClickHandler(new ClickHandler() {
+
+				@Override
+				public void onClick(ClickEvent event) {
+					diagramPanel.getPopupMenu().showPopupMenu(getEntity(), event);
+					hide();
+				}	
+			});
+		}
+		
+		private void addMouseOverHandler() {
+			infoIcon.addMouseOverHandler(new MouseOverHandler() {
+
+				@Override
+				public void onMouseOver(MouseOverEvent event) {
+					InfoIcon.this.getElement().getStyle().setCursor(Cursor.POINTER);
+					stopHideTimer();
+				}				
+			});
+		}
+		
+		private void addHideTimer() {
+			hideTimer = new HideTimer(); 
+		}					
+					
+		private class HideTimer extends Timer {
+			//private Boolean active = false;
+				
+			@Override
+			public void run() {
+				hide();
+				//active = false;
+			}
+			
+			public void schedule(int delayMillis) {
+				//if (!active) {	
+					//active = true;
+					super.schedule(delayMillis);
+				//}
+			}		
+		}
+	}	
 }
